780a068813a97de4ca13165b3d1c2a8598351f02
Isaac Bythewood <isaac@bythewood.me>
2019-05-27 17:52:37 -0400

    Add latest event context processor

--- artreart/settings.py
+++ artreart/settings.py
@@ -62,6 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'artreart.context_processors.latest_event',
             ],
         },
     },
