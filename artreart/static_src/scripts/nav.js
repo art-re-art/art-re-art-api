7793522585cba99e91eaffa959a3d6f00bbe0b72
Nancy VanNoppen <nancy@vannoppen.co>
2019-05-27 16:09:51 -0400

    nav and menu design

--- artreart/static_src/scripts/nav.js
+++ artreart/static_src/scripts/nav.js
@@ -4,32 +4,28 @@ $(document).ready(function() {
     if ($(this).scrollTop() > 50) {
       if (!$('.navbar').hasClass('navbar--scrolled')) {
         $('.navbar').addClass('navbar--scrolled');
-        // $('.navbar-brand .logo, .navbar-brand small').fadeOut(350);
-        $('.navbar-brand .logo').fadeOut(350);
       }
     } else {
       if ($('.navbar').hasClass('navbar--scrolled')) {
         $('.navbar').removeClass('navbar--scrolled');
-        // $('.navbar-brand .logo, .navbar-brand small').fadeIn(350);
-        $('.navbar-brand .logo').fadeIn(350);
       }
     }
   });
 
   // menu
-  $('.hamburger').on('click', function() {
+  $('#hamburger').on('click', function() {
     $(this).toggleClass('hamburger--clicked');
     $('#menu').slideToggle(350);
     if ($(this).hasClass('hamburger--clicked')) {
       $('body').css('overflow', 'hidden');
       $('.navbar').addClass('navbar--opened');
-      // $('.navbar-brand .logo, .navbar-brand small').fadeOut(350);
-      $('.navbar-brand .logo').fadeOut(350);
+      $('#hamburgerOpen').hide();
+      $('#hamburgerClose').show();
     } else {
       $('body').css('overflow', 'auto');
       $('.navbar').removeClass('navbar--opened');
-      // $('.navbar-brand .logo, .navbar-brand small').fadeIn(350);
-      $('.navbar-brand .logo').fadeIn(350);
+      $('#hamburgerOpen').show();
+      $('#hamburgerClose').hide();
     }
   });
 });
