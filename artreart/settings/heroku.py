37c231dfb0ef0e5bced707b1be0e47b00710b6ee
Isaac Bythewood <isaac@bythewood.me>
2019-05-27 20:41:12 -0400

    Add back in security redirect

--- artreart/settings/heroku.py
+++ artreart/settings/heroku.py
@@ -21,6 +21,16 @@ ALLOWED_HOSTS = [
 
 PREPEND_WWW = True
 
+SECURE_SSL_REDIRECT = True
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+
+USE_X_FORWARDED_HOST = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
 
 # Heroku
 # https://devcenter.heroku.com/articles/django-app-configuration
