231597520a077f8a000cf9a17078b582ce10fe4d
Isaac Bythewood <isaac@bythewood.me>
2019-05-29 18:43:45 -0400

    Update layouts on forms

--- forms/forms.py
+++ forms/forms.py
@@ -1,11 +1,41 @@
 from django import forms
 
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div
 
 from .models import ArtistSignup, ArtistSignupWork
 
 
 class ArtistSignupForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Div(
+                Div('name', css_class='col'),
+                css_class='row',
+            ),
+            Div(
+                Div('email', css_class='col'),
+                Div('phone_number', css_class='col'),
+                css_class='row',
+            ),
+            Div(
+                Div('city', css_class='col'),
+                Div('state', css_class='col'),
+                css_class='row',
+            ),
+            Div(
+                Div('website', css_class='col'),
+                Div('instagram', css_class='col'),
+                css_class='row',
+            ),
+            Div(
+                Div('artist_statement', css_class='col'),
+                css_class='row',
+            ),
+        )
+
     class Meta:
         model = ArtistSignup
         exclude = []
@@ -15,7 +45,21 @@ class ArtistSignupWorkForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.template = "bootstrap4/table_inline_formset.html"
+        self.helper.layout = Layout(
+            Div(
+                Div('title', css_class='col'),
+                Div('medium', css_class='col'),
+                css_class='row',
+            ),
+            Div(
+                Div('description', css_class='col'),
+                css_class='row',
+            ),
+            Div(
+                Div('special_installation_needs', css_class='col'),
+                css_class='row',
+            ),
+        )
 
     class Meta:
         model = ArtistSignupWork
@@ -23,5 +67,5 @@ class ArtistSignupWorkForm(forms.ModelForm):
 
 
 ArtistSignupWorkFormSet = forms.inlineformset_factory(
-    ArtistSignup, ArtistSignupWork, form=ArtistSignupWorkForm, extra=1, can_delete=False
+    ArtistSignup, ArtistSignupWork, form=ArtistSignupWorkForm, extra=3, can_delete=False
 )
