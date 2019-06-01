88c912873d94f647af25564499758f7e40c05800
Nancy VanNoppen <nancy@vannoppen.co>
2019-05-31 22:50:36 -0400

    override variables

--- antd.theme.js
+++ antd.theme.js
@@ -1,3 +1,9 @@
 module.exports = {
-  'primary-color': 'red'
+  'primary-color': 'red',
+  'font-family': '"Inconsolata", monospace',
+  'border-radius-base': 0,
+  'border-radius-sm': 0,
+  'btn-default-bg': 'black',
+  'btn-default-color': 'white',
+  'input-placeholder-color': 'white',
 }
