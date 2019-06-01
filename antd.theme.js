164b66d39b07b7db6aa744b9a831b0db67950f7c
Nancy VanNoppen <nancy@vannoppen.co>
2019-05-31 22:59:28 -0400

    override input variables

--- antd.theme.js
+++ antd.theme.js
@@ -3,7 +3,10 @@ module.exports = {
   'font-family': '"Inconsolata", monospace',
   'border-radius-base': 0,
   'border-radius-sm': 0,
+  'btn-shadow': 'none',
   'btn-default-bg': 'black',
   'btn-default-color': 'white',
   'input-placeholder-color': 'white',
+  'input-bg': 'red',
+  'input-color': 'white'
 }
