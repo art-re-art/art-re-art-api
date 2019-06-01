a375b19263be02734fb122a07729bda02f772024
Isaac Bythewood <isaac@bythewood.me>
2019-06-01 14:11:54 -0400

    Start adding forms for signup

---
+++ client/static_src/components/SignupArtistForm.js
@@ -0,0 +1,148 @@
+import React from "react";
+import {
+  Form,
+  Input,
+  Tooltip,
+  Icon,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  Button,
+  AutoComplete,
+  Typography
+} from "antd";
+
+const { Option } = Select;
+const AutoCompleteOption = AutoComplete.Option;
+const { Title, Paragraph } = Typography;
+
+
+class RegistrationForm extends React.Component {
+  state = {
+    confirmDirty: false
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
+
+  handleConfirmBlur = e => {
+    const value = e.target.value;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const { autoCompleteResult } = this.state;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 4 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 20 }
+      }
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0
+        },
+        sm: {
+          span: 16,
+          offset: 4
+        }
+      }
+    };
+    const prefixSelector = getFieldDecorator("prefix", {
+      initialValue: "1"
+    })(
+      <Select style={{ width: 70 }}>
+        <Option value="1">+1</Option>
+      </Select>
+    );
+
+    return (
+      <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+        <div style={{ padding: "3em 0" }}>
+          <Title level={2}>About You</Title>
+          <Paragraph>Tell us about you and your art!</Paragraph>
+        </div>
+        <Form.Item
+          label={
+            <span>
+              Name&nbsp;
+              <Tooltip title="What do you want others to call you?">
+                <Icon type="question-circle-o" />
+              </Tooltip>
+            </span>
+          }
+        >
+          {getFieldDecorator("nickname", {
+            rules: [
+              {
+                required: true,
+                message: "Please input your nickname!",
+                whitespace: true
+              }
+            ]
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label="E-mail">
+          {getFieldDecorator("email", {
+            rules: [
+              {
+                type: "email",
+                message: "The input is not valid E-mail!"
+              },
+              {
+                required: true,
+                message: "Please input your E-mail!"
+              }
+            ]
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label="Phone Number">
+          {getFieldDecorator("phone", {
+            rules: [
+              { required: true, message: "Please input your phone number!" }
+            ]
+          })(<Input addonBefore={prefixSelector} style={{ width: "100%" }} />)}
+        </Form.Item>
+        <Form.Item label="Website">
+          {getFieldDecorator("website", {
+            rules: [{ required: true, message: "Please input website!" }]
+          })(
+            <Input />
+          )}
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+          {getFieldDecorator("agreement", {
+            valuePropName: "checked"
+          })(
+            <Checkbox>
+              Art/Re/Art has my permissions to use me and my art for promotional purposes.
+            </Checkbox>
+          )}
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">
+            Register
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+export default Form.create({ name: "register" })(RegistrationForm);
