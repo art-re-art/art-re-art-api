60b5bf832802ac8544b09958c69c0af4b2d1cd25
Isaac Bythewood <isaac@bythewood.me>
2019-06-02 14:49:29 -0400

    Setup django-storages for s3 access on heroku

--- artreart/settings/heroku.py
+++ artreart/settings/heroku.py
@@ -46,3 +46,29 @@ BASE_URL = 'https://www.artreart.com'
 # Mailchimp
 
 MAILCHIMP_KEY = os.environ.get('MAILCHIMP_KEY', None)
+
+
+# django-storages
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', None)
+
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', None)
+
+AWS_STORAGE_BUCKET_NAME = 'static.artreart.com'
+
+AWS_DEFAULT_ACL = 'public-read'
+
+AWS_BUCKET_ACL = 'public-read'
+
+AWS_AUTO_CREATE_BUCKET = False
+
+AWS_QUERYSTRING_AUTH = False
+
+AWS_S3_FILE_OVERWRITE = False
+
+AWS_S3_CUSTOM_DOMAIN = 'static.artreart.com'
+
+MEDIA_URL = 'https://static.artreart.com/'
