edaf6c5a12477d4ef7b197887458937e6a1ff630
Isaac Bythewood <isaac@bythewood.me>
2019-06-03 19:12:17 -0400

    Fix card cover on event component

--- client/static_src/components/Event.js
+++ client/static_src/components/Event.js
@@ -8,17 +8,9 @@ const { Meta } = Card;
 export default class Event extends React.Component {
   render() {
     return (
-      <Col xl={8} lg={12} md={12} sm={24} style={{ padding: '1rem' }}>
+      <Col xl={8} lg={12} md={12} sm={24} style={{ padding: "1rem" }}>
         <Link to={`/events/${this.props.id}/`}>
-          <Card
-            hoverable
-            cover={
-              <div
-                style={{ textAlign: "center", backgroundImage: `url('${this.props.featured_image}')`, backgroundSize: "cover", padding: "5em" }}
-                // dangerouslySetInnerHTML={{ __html: this.props.featured_image }}
-              />
-            }
-          >
+          <Card hoverable cover={<img src={this.props.featured_image} />}>
             <Meta
               title={this.props.title}
               description={moment(this.props.datetime).format("LLLL")}
