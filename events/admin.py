715a751e5a09f54942ca933967915b0ac8d98825
Isaac Bythewood <isaac@bythewood.me>
2019-06-03 19:39:48 -0400

    Add inline image to event admin

--- events/admin.py
+++ events/admin.py
@@ -3,13 +3,20 @@ from django.contrib import admin
 from .models import Event, EventLocation, EventImage
 
 
+class EventImageInline(admin.StackedInline):
+    model = ArtistSignupWork
+    extra = 0
+
+
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    pass
+    inlines = [EventImageInline]
+
 
 @admin.register(EventImage)
 class EventImageAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(EventLocation)
 class EventLocationAdmin(admin.ModelAdmin):
