6e40f796eea7055f3602b41675e4884b52479adf
Isaac Bythewood <isaac@bythewood.me>
2019-06-06 20:19:50 -0400

    Update artist admin

--- artists/admin.py
+++ artists/admin.py
@@ -4,21 +4,32 @@ from .models import Artist, ArtistMedium, ArtistWork, ArtistWorkImage
 
 
 class ArtistWorkInline(admin.StackedInline):
-    filter_horizontal = ['medium',]
+    filter_horizontal = ["medium"]
     model = ArtistWork
-    extra = 0
+    extra = 1
 
 
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
-    list_display = ['name', 'instagram', 'website', 'medium_list', 'events_list']
-    list_filter = ['medium', 'events']
-    inlines = [ArtistWorkInline,]
+    list_display = ["name", "instagram", "website", "medium_list", "events_list"]
+    list_filter = ["medium", "events"]
+    inlines = [ArtistWorkInline]
+    filter_horizontal = ["medium", "events"]
 
 
 @admin.register(ArtistMedium)
 class ArtistMediumAdmin(admin.ModelAdmin):
     pass
+
+
+class ArtistWorkImageInline(admin.ModelAdmin):
+    model = ArtistWorkImage
+    extra = 1
+
+
+@admin.register(ArtistWork)
+class ArtistWorkAdmin(admin.ModelAdmin):
+    filter_horizontal = ["medium"]
 
 
 @admin.register(ArtistWorkImage)
