d95b43064dfcd8f0cf3765082f4d61bc6e1fb099
Isaac Bythewood <isaac@bythewood.me>
2019-06-15 20:51:21 +0000

    Add square to cropping

--- artreart/utils.py
+++ artreart/utils.py
@@ -9,10 +9,20 @@ def create_thumbnails(original_image):
         "quality": 95,
         "upscale": True,
     }
-    sizes = {"small": "640x360", "medium": "1280x720", "large": "1920x1080"}
+    sizes = {
+        "square": "640x640",
+        "small": "640x360",
+        "medium": "1280x720",
+        "large": "1920x1080",
+    }
     images = {}
     for size, resolution in sizes.items():
-        thumbnail = get_thumbnail(original_image, resolution, **thumbnail_kwargs)
+        if size == "square":
+            thumbnail = get_thumbnail(
+                original_image, resolution, crop="center center", **thumbnail_kwargs
+            )
+        else:
+            thumbnail = get_thumbnail(original_image, resolution, **thumbnail_kwargs)
         images[size] = {}
         images[size]["url"] = thumbnail.url
         try:
