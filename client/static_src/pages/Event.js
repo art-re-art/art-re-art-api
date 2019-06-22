17e4888f9b171978241303543c449e2b8e7951c3
Isaac Bythewood <isaac@bythewood.me>
2019-06-22 05:15:05 +0000

    Improved styles of the event page

--- client/static_src/pages/Event.js
+++ client/static_src/pages/Event.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactGA from "react-ga";
-import { Col, Row, Typography, Card } from "antd";
+import { Col, Row, Typography, Card, Divider } from "antd";
 import moment from "moment";
 
 import Loading from "../components/Loading";
@@ -45,89 +45,110 @@ export default class Event extends React.Component {
 
     return (
       <div className="container">
-        <Row>
-          <Col xl={8} lg={24}>
+        <Row style={{ marginBottom: "6em" }}>
+          <Col>
+            <div
+              style={{
+                position: "absolute",
+                zIndex: 2,
+                backgroundColor: "red",
+                marginTop: "calc(50vh - 100px)",
+                padding: "1em"
+              }}
+            >
+              <Title level={2} style={{ color: "white", marginBottom: 0 }}>
+                {this.state.event.title}
+              </Title>
+              <Paragraph style={{ color: "white", marginBottom: 0 }}>
+                {moment(this.state.event.datetime).format("LLLL")}
+              </Paragraph>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                right: 0,
+                width: "82mm",
+                background: "white",
+                marginTop: "calc(50vh - 61.5mm)",
+                zIndex: 2
+              }}
+              dangerouslySetInnerHTML={{ __html: this.state.event.qrcode }}
+            />
             <img
-              src={this.state.event.featured_image.medium.url}
-              style={{ width: "100%", height: "auto", padding: "1em" }}
+              src={this.state.event.featured_image.large.url}
+              style={{
+                width: "calc(100% + 4em)",
+                height: "50vh",
+                objectFit: "cover",
+                objectPosition: "center center",
+                margin: "-2em"
+              }}
             />
           </Col>
-          <Col xl={16} lg={24}>
-            <Row>
-              <Col span={24}>
-                <Title level={2}>{this.state.event.title}</Title>
-                <Paragraph>
-                  {moment(this.state.event.datetime).format("LLLL")}
-                </Paragraph>
-                <Paragraph>
-                  {this.state.event.location && this.state.event.location.title}
-                  <br />
-                  {this.state.event.location &&
-                    this.state.event.location.street}
-                </Paragraph>
-              </Col>
-              {/* <Col lg={12} md={24}>
-                <div
-                  style={{ textAlign: "center" }}
-                  dangerouslySetInnerHTML={{ __html: this.state.event.qrcode }}
-                />
-              </Col> */}
-            </Row>
-            <Paragraph>
-              {this.state.event.location && (
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: `
-                      <iframe
-                        width="100%"
-                        height="400"
-                        id="gmap_canvas"
-                        src=${this._getEmbedSrc()}
-                        frameborder="0"
-                        scrolling="no"
-                        marginheight="0"
-                        marginwidth="0"
-                      />`
-                  }}
-                />
-              )}
-            </Paragraph>
+        </Row>
+        <Row>
+          <Col>
+            {this.state.event.location && (
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    <iframe
+                      width="100%"
+                      height="400"
+                      id="gmap_canvas"
+                      src=${this._getEmbedSrc()}
+                      frameborder="0"
+                      scrolling="no"
+                      marginheight="0"
+                      marginwidth="0"
+                    />`
+                }}
+              />
+            )}
           </Col>
         </Row>
-        {this.state.event.images && (
-          <Row
-            gutter={24}
-            style={{ display: "flex", flexWrap: "wrap", alignItems: "center" }}
-          >
-            {this.state.event.images.map(image => {
-              return (
-                <Col
-                  xl={6}
-                  lg={8}
-                  md={12}
-                  sm={12}
-                  xs={24}
-                  key={image.id}
-                  style={{ marginBottom: 24 }}
-                >
-                  <Card
-                    className="event-img"
-                    cover={
-                      <img
-                        src={image.image.small.url}
-                        alt={image.description}
-                      />
-                    }
+        {this.state.event.images.length > 0 && (
+          <div style={{ marginTop: "1rem" }}>
+            <Divider />
+            <Row
+              gutter={24}
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                alignItems: "center"
+              }}
+            >
+              {this.state.event.images.map(image => {
+                return (
+                  <Col
+                    xl={6}
+                    lg={8}
+                    md={12}
+                    sm={12}
+                    xs={24}
+                    key={image.id}
+                    style={{ marginBottom: 24 }}
                   >
-                    <Meta title={image.description} />
-                  </Card>
-                </Col>
-              );
-            })}
-          </Row>
+                    <Card
+                      className="event-img"
+                      cover={
+                        <img
+                          src={image.image.small.url}
+                          alt={image.description}
+                        />
+                      }
+                    >
+                      <Meta title={image.description} />
+                    </Card>
+                  </Col>
+                );
+              })}
+            </Row>
+          </div>
         )}
         {this.state.event.artists && (
           <div style={{ marginTop: "1rem" }}>
+            <Divider />
             <Title level={2}>Participating Artists</Title>
             <Row
               style={{
