670f86732b6d7d584cea8cca352bbfd2f9d45dfd
Isaac Bythewood <isaac@bythewood.me>
2019-06-29 02:36:59 +0000

    Rearrange loading css

--- client/static_src/components/Loading/Loading.js
+++ client/static_src/components/Loading/Loading.js
@@ -6,16 +6,8 @@ import "./Loading.less";
 export default class Loading extends React.Component {
   render() {
     return (
-      <div
-        style={{
-          width: "100%",
-          height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        <Spin size="large" />
+      <div className="loading">
+        <Spin className="loading__spinner" size="large" />
       </div>
     );
   }
