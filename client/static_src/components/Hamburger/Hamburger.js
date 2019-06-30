25b80820234ccd4a169f392be1aba32cad5f4f49
Isaac Bythewood <isaac@bythewood.me>
2019-06-30 05:33:27 +0000

    Add panel transitions to hamburger open and close

--- client/static_src/components/Hamburger/Hamburger.js
+++ client/static_src/components/Hamburger/Hamburger.js
@@ -47,13 +47,40 @@ export default class Hamburger extends React.Component {
           type={this.state.overlayVisible ? "close" : "menu"}
           onClick={this.state.overlayVisible ? this._hide : this._show}
         />
+        <div className="hamburger__panels">
+          {Array(5)
+            .fill()
+            .map((_, i) => {
+              return (
+                <CSSTransition
+                  key={i}
+                  in={this.state.overlayVisible}
+                  timeout={2000}
+                  classNames="transition--growdown"
+                  unmountOnExit
+                >
+                  <div
+                    className={
+                      `panels__${i} ` +
+                      (!this.state.overlayVisible && `panels__${i}--delayed`)
+                    }
+                  ></div>
+                </CSSTransition>
+              );
+            })}
+        </div>
         <CSSTransition
           in={this.state.overlayVisible}
-          timeout={500}
+          timeout={2000}
           classNames="transition--fade"
           unmountOnExit
         >
-          <div className="hamburger__overlay">
+          <div
+            className={
+              "hamburger__overlay " +
+              (this.state.overlayVisible && "hamburger__overlay--delayed")
+            }
+          >
             <div className="overlay">
               <img src={Logo} alt="ART/RE/ART" className="overlay__logo" />
               <Link to="/about/" onClick={this._hide} className="overlay__link">
