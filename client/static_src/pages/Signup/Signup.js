04cecf81a4696a458113ece939d1ea85d91190d1
Nancy VanNoppen <nancy@vannoppen.co>
2019-09-05 14:32:02 +0000

    fix signup page background colors

--- client/static_src/pages/Signup/Signup.js
+++ client/static_src/pages/Signup/Signup.js
@@ -22,7 +22,7 @@ const { Step } = Steps;
 const { Panel } = Collapse;
 const { Title, Paragraph } = Typography;
 const { confirm } = Modal;
-const { ArtistForm, WorkForm } = Components;
+const { ArtistForm, WorkForm, Layout } = Components;
 
 export default class Signup extends React.Component {
   state = {
@@ -221,9 +221,9 @@ export default class Signup extends React.Component {
 
   render() {
     return (
-      <div className="container">
+      <div>
         <Prompt message={this._prompt} />
-        <Row style={{ marginBottom: "2em" }}>
+        <Row style={{ padding: "2em", background: "#f2f2f2" }}>
           <Col>
             <Steps current={this.state.current}>
               <Step title="Artist" />
@@ -233,179 +233,181 @@ export default class Signup extends React.Component {
             </Steps>
           </Col>
         </Row>
-        {this.state.current === 0 && (
-          <div className="signup-forms">
-            <Row gutter={24}>
-              <Col lg={16} md={24}>
-                <Title level={2}>Artist</Title>
-                <Paragraph>Tell us about you!</Paragraph>
-                <ArtistForm
-                  wrappedComponentRef={this._updateArtistForm}
-                  data={this.state.dataArtist}
-                />
-              </Col>
-              <Col lg={8} md={24}>
-                <div style={{ borderLeft: "2px solid red", paddingLeft: "1rem" }}>
-                  <Title level={3}>Things to Note</Title>
-                  <Paragraph>No submission fees.</Paragraph>
-                  <Paragraph>No commission fees.</Paragraph>
-                  <Paragraph>Artists are responsible for installing and uninstalling their own work.</Paragraph>
-                </div>
-              </Col>
-            </Row>
-            <Row style={{ marginTop: "2em" }}>
-              <Col span={12}>
-                {this.state.validationErrors && (
-                  <Paragraph style={{ color: "red" }}>
-                    There were validation errors in the form, please correct
-                    them and submit again!
-                  </Paragraph>
-                )}
-              </Col>
-              <Col span={12} style={{ textAlign: "right" }}>
-                <Button
-                  onClick={this._submitArtistForm}
-                  type="primary"
-                  size="large"
-                >
-                  Add works <Icon type="caret-right" />
-                </Button>
-              </Col>
-            </Row>
-          </div>
-        )}
-        {this.state.current === 1 && (
-          <div className="signup-forms">
-            <Row gutter={48}>
-              <Col>
-                <Title level={2}>Work</Title>
-                <Paragraph>Tell us about what you create!</Paragraph>
-                <Collapse
-                  accordion
-                  defaultActiveKey="0"
-                  activeKey={this.state.activeKey}
-                  onChange={this._changePanel}
-                >
-                  {this.state.dataWork.map((data, index) => {
-                    return (
-                      <Panel
-                        header={data.title ? data.title : "Work"}
-                        key={index}
-                        forceRender
+        <Layout.Container>
+          {this.state.current === 0 && (
+            <div className="signup-forms">
+              <Row gutter={24}>
+                <Col lg={16} md={24}>
+                  <Title level={2}>Artist</Title>
+                  <Paragraph>Tell us about you!</Paragraph>
+                  <ArtistForm
+                    wrappedComponentRef={this._updateArtistForm}
+                    data={this.state.dataArtist}
+                  />
+                </Col>
+                <Col lg={8} md={24}>
+                  <div style={{ borderLeft: "2px solid red", paddingLeft: "1rem" }}>
+                    <Title level={3}>Things to Note</Title>
+                    <Paragraph>No submission fees.</Paragraph>
+                    <Paragraph>No commission fees.</Paragraph>
+                    <Paragraph>Artists are responsible for installing and uninstalling their own work.</Paragraph>
+                  </div>
+                </Col>
+              </Row>
+              <Row style={{ marginTop: "2em" }}>
+                <Col span={12}>
+                  {this.state.validationErrors && (
+                    <Paragraph style={{ color: "red" }}>
+                      There were validation errors in the form, please correct
+                      them and submit again!
+                    </Paragraph>
+                  )}
+                </Col>
+                <Col span={12} style={{ textAlign: "right" }}>
+                  <Button
+                    onClick={this._submitArtistForm}
+                    type="primary"
+                    size="large"
+                  >
+                    Add works <Icon type="caret-right" />
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+          )}
+          {this.state.current === 1 && (
+            <div className="signup-forms">
+              <Row gutter={48}>
+                <Col>
+                  <Title level={2}>Work</Title>
+                  <Paragraph>Tell us about what you create!</Paragraph>
+                  <Collapse
+                    accordion
+                    defaultActiveKey="0"
+                    activeKey={this.state.activeKey}
+                    onChange={this._changePanel}
+                  >
+                    {this.state.dataWork.map((data, index) => {
+                      return (
+                        <Panel
+                          header={data.title ? data.title : "Work"}
+                          key={index}
+                          forceRender
+                        >
+                          <WorkForm
+                            wrappedComponentRef={this._updateWorkForms}
+                            data={data}
+                          />
+                        </Panel>
+                      );
+                    })}
+                  </Collapse>
+                  <Row style={{ marginTop: "2em" }}>
+                    <Col span={12}>
+                      <Button
+                        onClick={this._removeWorkForm}
+                        style={{ background: "black", color: "white" }}
                       >
-                        <WorkForm
-                          wrappedComponentRef={this._updateWorkForms}
-                          data={data}
-                        />
-                      </Panel>
-                    );
-                  })}
-                </Collapse>
-                <Row style={{ marginTop: "2em" }}>
-                  <Col span={12}>
-                    <Button
-                      onClick={this._removeWorkForm}
-                      style={{ background: "black", color: "white" }}
-                    >
-                      <Icon type="minus" /> Remove last
-                    </Button>
-                  </Col>
-                  <Col span={12} style={{ textAlign: "right" }}>
-                    <Button
-                      onClick={this._addWorkForm}
-                      style={{ background: "black", color: "white" }}
-                    >
-                      <Icon type="plus" /> Add another
-                    </Button>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-            <Row style={{ marginTop: "2em" }}>
-              <Col>
-                {this.state.validationErrors && (
-                  <Paragraph style={{ color: "red" }}>
-                    There were validation errors in the form, please correct
-                    them and submit again!
+                        <Icon type="minus" /> Remove last
+                      </Button>
+                    </Col>
+                    <Col span={12} style={{ textAlign: "right" }}>
+                      <Button
+                        onClick={this._addWorkForm}
+                        style={{ background: "black", color: "white" }}
+                      >
+                        <Icon type="plus" /> Add another
+                      </Button>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+              <Row style={{ marginTop: "2em" }}>
+                <Col>
+                  {this.state.validationErrors && (
+                    <Paragraph style={{ color: "red" }}>
+                      There were validation errors in the form, please correct
+                      them and submit again!
+                    </Paragraph>
+                  )}
+                </Col>
+              </Row>
+              <Row style={{ marginTop: "2em" }}>
+                <Col span={12}>
+                  <Button
+                    onClick={this._editArtistForm}
+                    type="primary"
+                    size="large"
+                  >
+                    <Icon type="caret-left" /> Edit artist
+                  </Button>
+                </Col>
+                <Col span={12} style={{ textAlign: "right" }}>
+                  <Button
+                    onClick={this._submitWorkForms}
+                    type="primary"
+                    size="large"
+                  >
+                    Confirm signup <Icon type="caret-right" />
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+          )}
+          {this.state.current === 2 && (
+            <div className="signup-forms">
+              <Row style={{ marginTop: "2em" }}>
+                <Col>
+                  <Title level={2}>Confirm</Title>
+                  <Paragraph>
+                    Are you sure you want to submit yourself, and your work, to
+                    Art/Re/Art?
                   </Paragraph>
-                )}
-              </Col>
-            </Row>
-            <Row style={{ marginTop: "2em" }}>
-              <Col span={12}>
-                <Button
-                  onClick={this._editArtistForm}
-                  type="primary"
-                  size="large"
-                >
-                  <Icon type="caret-left" /> Edit artist
-                </Button>
-              </Col>
-              <Col span={12} style={{ textAlign: "right" }}>
-                <Button
-                  onClick={this._submitWorkForms}
-                  type="primary"
-                  size="large"
-                >
-                  Confirm signup <Icon type="caret-right" />
-                </Button>
-              </Col>
-            </Row>
-          </div>
-        )}
-        {this.state.current === 2 && (
-          <div className="signup-forms">
-            <Row style={{ marginTop: "2em" }}>
-              <Col>
-                <Title level={2}>Confirm</Title>
-                <Paragraph>
-                  Are you sure you want to submit yourself, and your work, to
-                  Art/Re/Art?
-                </Paragraph>
-                <Paragraph>
-                  You retain full ownership of your work. Art/Re/Art reserves
-                  the right to use you and your art for marketing purposes. This
-                  includes, but is not limited to: Displaying you and your art
-                  on our website and on our app, including you and your art in
-                  press releases relating to Art/Re/Art, and adding you and your
-                  art to any print media that Art/Re/Art may produce.
-                </Paragraph>
-              </Col>
-            </Row>
-            <Row style={{ marginTop: "2em" }}>
-              <Col span={12}>
-                <Button
-                  onClick={this._editWorksForm}
-                  type="primary"
-                  size="large"
-                >
-                  <Icon type="caret-left" /> Edit signup
-                </Button>
-              </Col>
-              <Col span={12} style={{ textAlign: "right" }}>
-                <Button
-                  onClick={this._completeSignup}
-                  type="primary"
-                  size="large"
-                >
-                  I agree, complete signup <Icon type="caret-right" />
-                </Button>
-              </Col>
-            </Row>
-          </div>
-        )}
-        {this.state.current === 3 && (
-          <div className="signup-forms">
-            <Row style={{ marginTop: "2em" }}>
-              <Col>
-                <Title level={2}>Complete</Title>
-                <Paragraph>
-                  Thank you for signing up for Art/Re/Art, we will be in touch!
-                </Paragraph>
-              </Col>
-            </Row>
-          </div>
-        )}
+                  <Paragraph>
+                    You retain full ownership of your work. Art/Re/Art reserves
+                    the right to use you and your art for marketing purposes. This
+                    includes, but is not limited to: Displaying you and your art
+                    on our website and on our app, including you and your art in
+                    press releases relating to Art/Re/Art, and adding you and your
+                    art to any print media that Art/Re/Art may produce.
+                  </Paragraph>
+                </Col>
+              </Row>
+              <Row style={{ marginTop: "2em" }}>
+                <Col span={12}>
+                  <Button
+                    onClick={this._editWorksForm}
+                    type="primary"
+                    size="large"
+                  >
+                    <Icon type="caret-left" /> Edit signup
+                  </Button>
+                </Col>
+                <Col span={12} style={{ textAlign: "right" }}>
+                  <Button
+                    onClick={this._completeSignup}
+                    type="primary"
+                    size="large"
+                  >
+                    I agree, complete signup <Icon type="caret-right" />
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+          )}
+          {this.state.current === 3 && (
+            <div className="signup-forms">
+              <Row style={{ marginTop: "2em" }}>
+                <Col>
+                  <Title level={2}>Complete</Title>
+                  <Paragraph>
+                    Thank you for signing up for Art/Re/Art, we will be in touch!
+                  </Paragraph>
+                </Col>
+              </Row>
+            </div>
+          )}
+        </Layout.Container>
       </div>
     );
   }
